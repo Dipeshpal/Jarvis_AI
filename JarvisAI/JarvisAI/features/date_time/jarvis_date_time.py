a38a200c8274d452cebc0f44b3c1337a165a24a5
Dipesh <dipeshpal17@gmail.com>
2020-09-02 03:35:53 +0530

    0.0.6

--- JarvisAI/JarvisAI/features/date_time/jarvis_date_time.py
+++ JarvisAI/JarvisAI/features/date_time/jarvis_date_time.py
@@ -2,21 +2,24 @@
 import datetime
 
 
-# do not remove inp=None the function format should be same, name can be change
+# do not remove data=None, model=None the function format should be same, name can be change
 # inp is the input from the user (query ask by user)
 # you can use this inp inside the function. LOL, because you may need user input
 # Function should return something because this is the output we want to show user
 def tell_me_date(data=None, model=None):
-    # inp = inp_and_userconfig['inp']  # user input as string
-    # user_config = inp_and_userconfig['user_config']  # user_config as dict
-    # user_config = {
-    #     "name": "Sir",
-    #     "age": 22,
-    #     "city": "Indore"
-    # }
-    # inp (do something if you want to do with inp)
+    """
+    Do not remove data=None, model=None
+    data is:
+    data = {
+            'user_input': inp, # input from user
+            'features_config': jarvis_obj.features_config, # this will use to find the correct funtion which needs to be call
+            'user_config': jarvis_obj.user_config, # user releated configurations. Just print it for more details.
+            'DEV_MODE': DEV_MODE, # True / False is only possible values
+            'jarvis_obj': jarvis_obj # jarvis_obj is object of Jarvis class. Read the jarvis.py for more details.
+        }
+    """
     date = datetime.datetime.now().strftime("%b %d %Y")
-    # the return format should be string or integer only
+    # the return format should be string or integer only. The only one return should be here.
     return date
 
 
